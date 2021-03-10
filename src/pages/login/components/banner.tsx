75cf99ce7d55b26e0e722581be6a8621586b2650
puhao <379360444@qq.com>
2021-03-10 18:52:48 +0800

    feat: 优化banner图加载

--- src/pages/login/components/banner.tsx
+++ src/pages/login/components/banner.tsx
@@ -5,7 +5,7 @@ export default () => {
         <img src={require('@img/login/logo.png')} />
       </div>
       <div className="login-banner">
-        <img src={require('@img/login/banner.png')} />
+        <img src="https://91trial.oss-cn-shanghai.aliyuncs.com/91trialTest/f3b4c1f1cb0c4888820049f85c07bbe2.png" />
       </div>
     </>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useBoolean } from 'ahooks';
 
 import './index.scss';
 
@@ -14,7 +15,11 @@ import Phone from './components/phone';
 export default () => {
   const [loginFlag, setLoginFlag] = useState(true);
 
-  const Root = (
+  const [imgLoad, { setTrue: setImgLoad }] = useBoolean(false);
+
+  imgOnload();
+
+  const Root = imgLoad && (
     <div className="body login">
       <div className="login-body">
         <div className="login-left">
@@ -41,7 +46,7 @@ export default () => {
               {loginFlag ? <Account></Account> : <Phone></Phone>}
             </div>
             <div className="login-footer">
-              <span>忘记密码</span>
+              <span>忘记密码?</span>
               <span>注册</span>
             </div>
           </div>
@@ -53,6 +58,22 @@ export default () => {
   //set active class
   function getActiveClass(flag: Boolean) {
     return flag ? 'login-tab__item login-tab__item--active' : 'login-tab__item';
+  }
+
+  function imgOnload() {
+    let num = 0;
+    [
+      'https://91trial.oss-cn-shanghai.aliyuncs.com/91trialTest/f3b4c1f1cb0c4888820049f85c07bbe2.png',
+    ].forEach((v, idx, arr) => {
+      const img = new Image();
+      img.src = v;
+      img.onload = () => {
+        num++;
+        if (num == arr.length) {
+          setImgLoad();
+        }
+      };
+    });
   }
 
   return Root;
