1b053885cce6858ec8be2a1762c14c14afe2f4c7
puhao <379360444@qq.com>
2021-03-10 18:18:44 +0800

    chore: add alias

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'umi';
 import routes from './src/router/index';
 
+//iconfont online src https://www.iconfont.cn/manage/index?spm=a313x.7781069.1998910419.12&manage_type=myprojects&projectId=2409301&keyword=&project_type=&page=
+const iconHref = '';
+
 export default defineConfig({
   hash: true,
   mountElementId: 'App',
+  links: [
+    // {
+    //   rel: 'stylesheet',
+    //   href: iconHref,
+    // },
+  ],
   theme: {
     // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
     hack: `true; @import "~@/assets/css/antbase/base.less";`,
@@ -21,5 +30,8 @@ export default defineConfig({
     title: false,
   },
   routes: routes,
+  alias: {
+    '@img': '@/assets/images',
+  },
   fastRefresh: {},
 });
